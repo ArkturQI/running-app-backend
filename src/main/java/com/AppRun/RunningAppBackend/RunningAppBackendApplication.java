8925432d3a3848c531dea80ee05aac8ce1379b51
ArturKomar <komar-artur@mail.ru>
2026-03-13 15:02:03 +0700

    Add all project files

---
+++ src/main/java/com/AppRun/RunningAppBackend/RunningAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.AppRun.RunningAppBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunningAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RunningAppBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/AppRun/RunningAppBackend/RunningAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AppRun.RunningAppBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunningAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
